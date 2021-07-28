88067691ef412041d12c24f149737b5a994bd3fb
Arjun Chib <arjunpatrickchib@gmail.com>
2021-07-27 23:52:24 -0400

    Add meme component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <SearchBar v-model:matches="matches" :memes="memes" />
-  {{ matches }}
+  <Meme v-for="meme of matches" :key="meme.id" :meme="meme" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import axios from "axios";
 import SearchBar from "./components/SearchBar.vue";
+import Meme from "./components/Meme.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     SearchBar,
+    Meme,
   },
   data() {
     return {
---
+++ src/components/Meme.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>{{ meme.name }}</h2>
+  <ul>
+    <li v-for="cmd of meme.commands" :key="cmd">{{ cmd }}</li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Meme",
+  props: {
+    meme: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
